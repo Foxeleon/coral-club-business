8518fc9049e750b0efdb81722ed215380df8bdd7
Foxeleon <andreywirz@gmail.com>
2025-09-15 11:45:42 +0200

    server hotfixes

--- server.production.js
+++ server.production.js
@@ -1,8 +1,9 @@
 import createServer from './dist/server/server.js';
 
 process.env.NODE_ENV = 'test';
+const projectRoot = process.cwd();
 
-createServer().then(({ app }) => {
+createServer(projectRoot).then(({ app }) => {
     const port = process.env.PORT || 8080;
     app.listen(port, () => {
         console.log(`Server started on port ${port}`);
