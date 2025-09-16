001705a5d55cd5ab07f1a2ca8b5f50ba2467cb7a
Foxeleon <andreywirz@gmail.com>
2025-09-16 19:41:44 +0200

    ssr hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
