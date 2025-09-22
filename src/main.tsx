b308c5aa98e334a02dbdf594650be39a91fde5d7
Foxeleon <andreywirz@gmail.com>
2025-09-22 23:19:26 +0200

    fix of dev env

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,26 @@ import { getLanguageFromPath } from './utils/language-helper';
 import './index.css';
 import './i18n';
 
-// Defining the language and the base path BEFORE rendering
 const language = getLanguageFromPath(window.location.pathname);
 const basename = language ? `/${language}` : '/';
 
-ReactDOM.hydrateRoot(
-    document.getElementById('root') as HTMLElement,
+const rootElement = document.getElementById('root') as HTMLElement;
+
+const app = (
     <React.StrictMode>
         <BrowserRouter basename={basename}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
 );
+
+// Conditional rendering
+// import.meta.env.DEV is a special Vite variable that is set to true in development mode.
+if (import.meta.env.DEV) {
+    // In development mode, we use createRoot for rendering from scratch.
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(app);
+} else {
+    // In production mode (after build), we use hydrateRoot to "animate" SSG content.
+    ReactDOM.hydrateRoot(rootElement, app);
+}
